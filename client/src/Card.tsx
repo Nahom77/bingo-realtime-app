1646932631e32117f12ca2dda4d1597281b411c7
Nahom Tigistu <nahomtigistu777@gmail.com>
2025-07-25 02:06:23 +0300

    Main app styled using gradient colors

--- client/src/Card.tsx
+++ client/src/Card.tsx
@@ -9,17 +9,26 @@ const Card = ({ number, drawnNums }: Props) => {
   return (
     <div
       style={{
-        width: '30px',
-        height: '30px',
+        width: 'clamp(10px, 10px + 1vw, 30px)',
+        height: 'clamp(10px, 10px + 1vw, 30px)',
         padding: '20px',
-        border: isDrawn ? '4px solid black' : '1px solid black',
+        border: isDrawn
+          ? '2px solid rgba(46, 45, 45, 0.79)'
+          : '1px solid rgba(233, 228, 228, 0.79)',
         borderRadius: '10px',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+        // color: '#000000',
+        background: isDrawn ? 'rgba(0, 0, 0, 0.466)' : '',
+        // border: '1px solid rgba(0, 0, 0, 0)',
+        boxShadow: '0 1rem 2rem rgba(0, 0, 0, 0.466)',
+        transition: 'all 0.1s ease',
       }}
     >
-      <h2>{number}</h2>
+      <h2 style={{ color: isDrawn ? 'white' : 'rgba(0, 0, 0, 0.98)' }}>
+        {number}
+      </h2>
     </div>
   );
 };
